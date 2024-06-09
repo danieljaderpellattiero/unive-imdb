f7639d192e9974f23786d62b8cd82d48963cbc8c
Michele Lotto <875922@stud.unive.it>
2024-06-09 15:57:33 +0200

    inserimento di prova

--- test_queries/queries_creator.py
+++ test_queries/queries_creator.py
@@ -4,7 +4,7 @@ from random import choices
 from copy import deepcopy
 from tqdm import tqdm
 
-@profile
+#@profile
 async def main()->list[dict]:
     
     async with Surreal("ws://localhost:8000/rpc") as db:
@@ -15,18 +15,30 @@ async def main()->list[dict]:
         movies:list[dict] = await db.select('title_ratings')
         
         movies = data_augmentation(movies)
+    
+    return movies
+
+async def prova_inserimento(movies:list[dict]):
+    
+    for movie in tqdm(movies):
+        del movie['id']
+    
+    async with Surreal("ws://localhost:8000/rpc") as db:
         
-        #weights + sample with replacement
-        weights = [movie.get('numVotes') for movie in movies]
-        to_query = choices(movies, weights = weights, k = 10000)
+        await db.use("imb", "imb")
+        await db.signin({"user": "whoami", "pass": "root"})
         
-        #print(sum(x['numVotes'] for x in to_query)/len(to_query)) #for weighting testing
+        movies_chunked = [movies[i:i+14000] for i in range(0, len(movies), 14000)]
+        await db.query(sql=f"create prova")
         
-        ids = [movie.get('id') for movie in to_query]
+        for chunk in tqdm(movies_chunked):
+            #print(len(f"create prova; INSERT INTO prova {chunk}")) #16777216
+            bho=await db.query(sql=f"INSERT INTO prova {chunk}")
+            #print(bho)
         
-        print(ids)
-            
-    return ids
+        #movies:list[dict] = await db.select('prova')
+        
+        #print(len(movies))
 
 def data_augmentation(movies:list[dict])->list[dict]:
     for _ in tqdm(range(14), desc="data augmentation", leave=True):
@@ -37,8 +49,23 @@ def data_augmentation(movies:list[dict])->list[dict]:
     
     return movies
 
+def sample_movies(movies:list[dict])->list[dict]:
+    weights = [movie.get('numVotes') for movie in movies]
+    to_query = choices(movies, weights = weights, k = 10000)
+    
+    #print(sum(x['numVotes'] for x in to_query)/len(to_query)) #for weighting testing
+        
+    #ids = [movie.get('id') for movie in to_query]
+    
+    #print(ids)
+    
+    return to_query
+
 if __name__ == "__main__":
     import asyncio
 
     movies = asyncio.run(main())
     
+    asyncio.run(prova_inserimento(movies))
+    
+    #sample_movies(movies)
