6f16ba9ff8309957380122c5185260b9342c1a2b
Michele Lotto <875922@stud.unive.it>
2024-06-11 19:48:45 +0200

    updated script

--- parquet/parquet_to_tsv.py
+++ parquet/parquet_to_tsv.py
@@ -6,8 +6,9 @@ import os
 from tqdm import tqdm
 
 for (dirpath, dirnames, filenames) in walk(os.getcwd()):
-    for filename in tqdm(filenames):
-        if filename.endswith('.parquet'):
-            df = pd.read_parquet(filename)
-            df.to_csv(filename.replace('.parquet', '.tsv'), sep="\t", index= True if ("name.basics" in filename or "title.basics" in filename or "title.episodes" in filename) else False)
+    for filename in (pbar :=tqdm([val for val in filenames if val.endswith('.parquet')])):
+        pbar.set_description(f"Processing '{filename}'")
+        df = pd.read_parquet(filename)
+        df.to_csv(filename.replace('.parquet', '.tsv'), sep="\t", index= True if ("name.basics" in filename or "title.basics" in filename or "title.episodes" in filename) else False)
+        tqdm.write(f"'{filename}': Done!")
     break
