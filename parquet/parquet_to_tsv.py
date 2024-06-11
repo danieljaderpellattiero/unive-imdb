89f95bfb6c1eb7cdc30d857f6eea51bec1f96917
Daniel Jader Pellattiero <danieljaderpellattiero@outlook.com>
2024-06-11 17:18:08 +0200

    Filenames consistency adjusted.

--- parquet/parquet_to_tsv.py
+++ parquet/parquet_to_tsv.py
@@ -9,5 +9,5 @@ for (dirpath, dirnames, filenames) in walk(os.getcwd()):
     for filename in tqdm(filenames):
         if filename.endswith('.parquet'):
             df = pd.read_parquet(filename)
-            df.to_csv(filename.replace('.parquet', '.tsv'), sep="\t", index= True if ("name.basics" in filename or "title.basics" in filename or "title.episode" in filename) else False)
+            df.to_csv(filename.replace('.parquet', '.tsv'), sep="\t", index= True if ("name.basics" in filename or "title.basics" in filename or "title.episodes" in filename) else False)
     break
