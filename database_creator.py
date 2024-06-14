4b96e32a9d0856ef6cd378f61fb7e138ce7bd7ac
Michele Lotto <875922@stud.unive.it>
2024-06-14 23:24:13 +0200

    script to create database

--- database_creator.py
+++ database_creator.py
@@ -1,42 +1,50 @@
 import pandas as pd
-from pymongo import MongoClient
+from pymongo import MongoClient, ASCENDING
 import json
 
 import argparse
 
-from tqdm import tqdm
+from tqdm.contrib.concurrent import process_map
+
+import gc
+
 
 collectionNames=["name.basics","title.akas","title.basics","title.crew","title.episodes","title.principals"]
+parquet_path = "data_preparation/parquet/"
 
-def importCollection(db, parquet_path, coll_name):
+def importCollection(coll_name):
    
    """ Imports a csv file at path csv_name to a mongo colection
    returns: count of the documants in the new collection
    """
    
-   coll = db[coll_name]
-      
-   with open(parquet_path) as f:
-      payload = json.load(f)
-   
-   coll.insert_many(payload)
-
-def mongoimport(collectionName:str=None):
-   
    client = MongoClient(host=db_url, port=db_port)
    db = client[db_name]
    
+   coll = db[coll_name]
    
-   parquet_path = "data_preparation/parquet/"
+   with open(parquet_path+coll_name+".json") as f:
+      payload = json.load(f)
    
    try:
-      if collectionName is not None:
-         importCollection(db, parquet_path+collectionName+".json", collectionName)
-      else:
-         for collectionName in tqdm(collectionNames):
-            importCollection(db, parquet_path+collectionName+".json", collectionName)
+      coll.insert_many(payload, ordered=False)
+      del payload
+      gc.collect()
+      
+      if "title.akas" in coll_name:
+         coll.create_index([("nameLower",ASCENDING)], background=True)
+      
+      if "title.principals" in coll_name:
+         coll.create_index([("titleId",ASCENDING)], background=True)
    finally:
       client.close()
+
+def mongoimport(collectionName:str=None):
+
+   if collectionName is not None:
+      importCollection(parquet_path+collectionName+".json", collectionName)
+   else:
+      process_map(importCollection, collectionNames, max_workers=workers)
       
 def mongodrop(collectionName:str=None):
    
@@ -53,18 +61,16 @@ def mongodrop(collectionName:str=None):
 
 if __name__ == "__main__":
    
+   parser = argparse.ArgumentParser(description='Utility script to perform DB operations',formatter_class=argparse.RawTextHelpFormatter)
    
-   class CustomFormatter(argparse.RawTextHelpFormatter, argparse.ArgumentDefaultsHelpFormatter):
-      pass
+   optional=parser.add_argument_group('Optional arguments')
+   optional.add_argument('-workers', help='Number of workers to use for the import (default 1)', action='store', type=int, default=1, metavar='workers', choices=range(1,6+1))
+   optional.add_argument('-db_url', help='URL of the MongoDB server (default localhost)', action='store', type=str, default='localhost', metavar='db_url')
+   optional.add_argument('-db_port', help='Port of the MongoDB server (default 27017)', action='store', type=int, default=27017, metavar='db_port')
+   optional.add_argument('-db_name', help='Name of the MongoDB database (default unive-imb)', action='store', type=str, default='unive-imb', metavar='db_name')
    
-   parser = argparse.ArgumentParser(description='Utility script to perform DB operations',formatter_class=CustomFormatter)
-   
-   db_connection=parser.add_argument_group('DB connection parameters')
-   db_connection.add_argument('--db_url', help='URL of the MongoDB server', action='store', type=str, default='localhost', metavar='db_url')
-   db_connection.add_argument('--db_port', help='Port of the MongoDB server', action='store', type=int, default=27017, metavar='db_port')
-   db_connection.add_argument('--db_name', help='Name of the MongoDB database', action='store', type=str, default='unive-imb', metavar='db_name')
-   
-   action_group=parser.add_mutually_exclusive_group(required=True)
+   action_group=parser.add_argument_group('Action to perform (choose one)')
+   action_group=action_group.add_mutually_exclusive_group(required=True)
    
    action_group.add_argument('--removeAll', help='Remove the entire DB', action='store_true')
    action_group.add_argument('--importAll', help='Create and populate the entire DB', action='store_true')
@@ -74,10 +80,12 @@ if __name__ == "__main__":
    
    args = parser.parse_args()
    
+   global workers
    global db_url
    global db_port
    global db_name
    
+   workers=args.workers
    db_url=args.db_url
    db_port=args.db_port
    db_name=args.db_name
